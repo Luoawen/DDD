351995f9cf9e118b905d3d63a3b45ce352ba3513
fanjc <dmkitwm@126.com>
2017-10-19 16:29:11 +0800

    merge local_scm_fanjc branch and add payordercmd.java

---
+++ m2c.scm.application/src/main/java/cn/m2c/scm/application/order/command/PayOrderCmd.java
@@ -0,0 +1,41 @@
+package cn.m2c.scm.application.order.command;
+
+import cn.m2c.common.MCode;
+import cn.m2c.common.StringUtil;
+import cn.m2c.ddd.common.AssertionConcern;
+import cn.m2c.scm.domain.NegativeException;
+/***
+ * 支付请求命令
+ * @author fanjc
+ * created date 2017年10月19日
+ * copyrighted@m2c
+ */
+public class PayOrderCmd extends AssertionConcern {
+	
+	private String orderId;
+	
+	private String userId;
+	
+	public PayOrderCmd(String orderNo, String userId) throws NegativeException {
+		
+		orderId = orderNo;
+		
+		this.userId = userId;
+		
+		if (StringUtil.isEmpty(orderId)) {
+			throw new NegativeException(MCode.V_1, "订单号参数为空(orderId)！");
+		}
+		
+		if (StringUtil.isEmpty(userId)) {
+			throw new NegativeException(MCode.V_1, "用户ID参数为空(userId)！");
+		}
+	}
+
+	public String getOrderId() {
+		return orderId;
+	}
+
+	public String getUserId() {
+		return userId;
+	}	
+}
