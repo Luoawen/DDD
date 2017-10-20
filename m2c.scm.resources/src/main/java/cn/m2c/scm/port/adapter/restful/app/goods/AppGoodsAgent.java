80b7f46942ae9dcd6ba3231f993715d4c704097e
owen <huc@lcjh.com>
2017-10-20 16:18:47 +0800

    APP商品详情

--- m2c.scm.resources/src/main/java/cn/m2c/scm/port/adapter/restful/app/goods/AppGoodsAgent.java
+++ m2c.scm.resources/src/main/java/cn/m2c/scm/port/adapter/restful/app/goods/AppGoodsAgent.java
@@ -1,11 +1,16 @@
 package cn.m2c.scm.port.adapter.restful.app.goods;
 
+import cn.m2c.common.JsonUtils;
 import cn.m2c.common.MCode;
 import cn.m2c.common.MPager;
 import cn.m2c.common.MResult;
+import cn.m2c.scm.application.goods.query.GoodsGuaranteeQueryApplication;
 import cn.m2c.scm.application.goods.query.GoodsQueryApplication;
 import cn.m2c.scm.application.goods.query.data.bean.GoodsBean;
+import cn.m2c.scm.application.goods.query.data.bean.GoodsGuaranteeBean;
 import cn.m2c.scm.application.goods.query.data.representation.GoodsGuessRepresentation;
+import cn.m2c.scm.application.goods.query.data.representation.app.AppGoodsDetailRepresentation;
+import cn.m2c.scm.application.unit.query.UnitQuery;
 import cn.m2c.scm.domain.service.goods.GoodsService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,6 +22,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.Writer;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -36,6 +42,10 @@ public class AppGoodsAgent {
     GoodsQueryApplication goodsQueryApplication;
     @Autowired
     GoodsService goodsService;
+    @Autowired
+    GoodsGuaranteeQueryApplication goodsGuaranteeQueryApplication;
+    @Autowired
+    UnitQuery unitQuery;
 
     /**
      * 商品猜你喜欢
@@ -84,5 +94,55 @@ public class AppGoodsAgent {
         return new ResponseEntity<MPager>(result, HttpStatus.OK);
     }
 
+    /**
+     * 查询商品详情
+     *
+     * @param goodsId
+     * @return
+     */
+    @RequestMapping(value = "/detail", method = RequestMethod.GET)
+    public ResponseEntity<MResult> queryGoodsDetailApp(
+            @RequestParam(value = "goodsId", required = false) String goodsId
+    ) {
+        MResult result = new MResult(MCode.V_1);
+        try {
+            GoodsBean goodsBean = goodsQueryApplication.appGoodsDetailByGoodsId(goodsId);
+            if (null != goodsBean) {
+                List<GoodsGuaranteeBean> goodsGuarantee = goodsGuaranteeQueryApplication.queryGoodsGuaranteeByIds(JsonUtils.toList(goodsBean.getGoodsGuarantee(), String.class));
+                String goodsUnitName = unitQuery.getUnitNameByUnitId(goodsBean.getGoodsUnitId());
+                AppGoodsDetailRepresentation representation = new AppGoodsDetailRepresentation(goodsBean,
+                        goodsGuarantee, goodsUnitName);
+                result.setContent(representation);
+            }
+            result.setStatus(MCode.V_200);
+        } catch (Exception e) {
+            LOGGER.error("queryGoodsDetailApp Exception e:", e);
+            result = new MResult(MCode.V_400, "查询商品详情失败");
+        }
+        return new ResponseEntity<MResult>(result, HttpStatus.OK);
+    }
+
+    /**
+     * 查询商品图文详情
+     *
+     * @param writer
+     * @param goodsId
+     */
+    @RequestMapping(value = "/desc", method = RequestMethod.GET)
+    public void appGoodsDesc(Writer writer,
+                             @RequestParam(value = "goodsId", required = true) String goodsId) {
+        try {
+            GoodsBean goodsBean = goodsQueryApplication.appGoodsDetailByGoodsId(goodsId);
+            StringBuffer sb = new StringBuffer();
+            sb.append("<!doctype html><html><head><meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\"><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no\" /><style>img{max-width:100%;border:0; margin : 0;padding :0 ;vertical-align:top;}</style>");
+            sb.append(goodsBean.getGoodsDesc());
+            writer.write(sb.toString());
+            writer.close();
+        } catch (IllegalArgumentException e) {
+            LOGGER.error("appGoodsDesc Exception e:", e);
+        } catch (Exception e) {
+            LOGGER.error("appGoodsDesc Exception e:", e);
+        }
+    }
 
 }
