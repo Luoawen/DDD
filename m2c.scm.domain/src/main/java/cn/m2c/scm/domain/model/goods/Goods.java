e47d256c2950b584d4541d039c4f6fd92230b530
owen <huc@lcjh.com>
2017-11-07 14:45:26 +0800

    商品上下架

--- m2c.scm.domain/src/main/java/cn/m2c/scm/domain/model/goods/Goods.java
+++ m2c.scm.domain/src/main/java/cn/m2c/scm/domain/model/goods/Goods.java
@@ -1,19 +1,19 @@
 package cn.m2c.scm.domain.model.goods;
 
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-import java.util.Map;
-
 import cn.m2c.ddd.common.domain.model.ConcurrencySafeEntity;
 import cn.m2c.ddd.common.domain.model.DomainEventPublisher;
 import cn.m2c.ddd.common.serializer.ObjectSerializer;
 import cn.m2c.scm.domain.model.goods.event.GoodsApproveAddEvent;
 import cn.m2c.scm.domain.model.goods.event.GoodsDeleteEvent;
-import cn.m2c.scm.domain.model.goods.event.GoodsOrDealerNameChangedEvent;
 import cn.m2c.scm.domain.model.goods.event.GoodsOffShelfEvent;
+import cn.m2c.scm.domain.model.goods.event.GoodsOrDealerNameChangedEvent;
 import cn.m2c.scm.domain.model.goods.event.GoodsUpShelfEvent;
 import cn.m2c.scm.domain.util.GetMapValueUtils;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
 
 /**
  * 商品
@@ -423,5 +423,17 @@ public class Goods extends ConcurrencySafeEntity {
      */
     public void launchGoods() {
     	this.goodsLaunchStatus = 1;
+    }
+
+    public String recognizedId() {
+        return recognizedId;
+    }
+
+    public String recognizedUrl() {
+        return recognizedUrl;
+    }
+
+    public Integer goodsStatus() {
+        return goodsStatus;
     }
 }
