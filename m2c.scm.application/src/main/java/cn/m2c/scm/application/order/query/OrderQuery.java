16167d0e4114a6ba5629666962f49213ea06fc12
fanjc <dmkitwm@126.com>
2017-11-09 21:55:14 +0800

    aaa

--- m2c.scm.application/src/main/java/cn/m2c/scm/application/order/query/OrderQuery.java
+++ m2c.scm.application/src/main/java/cn/m2c/scm/application/order/query/OrderQuery.java
@@ -376,7 +376,7 @@ public class OrderQuery {
 		
 		sql.delete(0, sql.length());
 		sql.append(" SELECT order_id  orderId , dealer_id  dealerId,dealer_order_id dealerOrderId \r\n");
-		sql.append("  ,dealer_discount  dealerDiscount, _status  status, res_rate resRate\r\n");
+		sql.append("  ,dealer_discount  dealerDiscount, _status  status\r\n");
 		sql.append("  ,goods_amount goodsAmount, order_freight orderFreight,plateform_discount plateformDiscount\r\n");
 		sql.append("  ,dealer_discount dealerDiscount, term_of_payment termOfPayment\r\n");
 		sql.append(" FROM t_scm_order_dealer WHERE order_id=?");
@@ -385,7 +385,7 @@ public class OrderQuery {
 		//获取商品
 		sql.delete(0, sql.length());
 		sql.append("SELECT dealer_order_id, _status, freight, plateform_discount, goods_amount, rate,goods_id, sku_id, sku_name,supply_price, discount_price, \r\n")
-		.append(" sell_num, bds_rate, media_id, media_res_id, saler_user_id, saler_user_rate, is_change, change_price\r\n")
+		.append(" sell_num, bds_rate, media_id, media_res_id, saler_user_id, saler_user_rate, is_change, change_price, res_rate, marketing_id,market_level\r\n")
 		.append(" FROM t_scm_order_detail WHERE order_id=? ORDER BY dealer_order_id");
 		List<OrderGoodsBean> ls = supportJdbcTemplate.queryForBeanList(sql.toString(), OrderGoodsBean.class, orderNo);
 		
