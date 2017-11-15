3a29ac4ec13224454ec043399b1db24fe9932cf0
fanjc <dmkitwm@126.com>
2017-11-15 20:32:35 +0800

    ok

--- m2c.scm.resources/src/main/java/cn/m2c/scm/port/adapter/restful/app/order/AppOrderAgent.java
+++ m2c.scm.resources/src/main/java/cn/m2c/scm/port/adapter/restful/app/order/AppOrderAgent.java
@@ -33,8 +33,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.alibaba.fastjson.JSONObject;
-
 import java.util.List;
 
 /***
