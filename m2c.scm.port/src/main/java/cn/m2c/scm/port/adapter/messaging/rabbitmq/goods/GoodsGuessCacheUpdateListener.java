e6e53f30ff7b97bc374c5f3b6d98252f836583e2
owen <huc@lcjh.com>
2017-11-16 17:24:23 +0800

    猜你喜欢更新缓存

---
+++ m2c.scm.port/src/main/java/cn/m2c/scm/port/adapter/messaging/rabbitmq/goods/GoodsGuessCacheUpdateListener.java
@@ -0,0 +1,39 @@
+package cn.m2c.scm.port.adapter.messaging.rabbitmq.goods;
+
+import cn.m2c.ddd.common.application.configuration.RabbitmqConfiguration;
+import cn.m2c.ddd.common.event.ConsumedEventStore;
+import cn.m2c.ddd.common.notification.NotificationReader;
+import cn.m2c.ddd.common.port.adapter.messaging.rabbitmq.ExchangeListener;
+import cn.m2c.scm.application.goods.query.GoodsQueryApplication;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.orm.hibernate4.HibernateTransactionManager;
+
+/**
+ * 商品下架更新猜你喜欢缓存
+ */
+public class GoodsGuessCacheUpdateListener extends ExchangeListener {
+    public GoodsGuessCacheUpdateListener(RabbitmqConfiguration rabbitmqConfiguration, HibernateTransactionManager hibernateTransactionManager, ConsumedEventStore consumedEventStore) {
+        super(rabbitmqConfiguration, hibernateTransactionManager, consumedEventStore);
+    }
+
+    @Autowired
+    GoodsQueryApplication goodsQueryApplication;
+
+    @Override
+    protected String packageName() {
+        return this.getClass().getPackage().getName();
+    }
+
+    @Override
+    protected void filteredDispatch(String aType, String aTextMessage) throws Exception {
+        NotificationReader reader = new NotificationReader(aTextMessage);
+        String goodsId = reader.eventStringValue("goodsId");
+        goodsQueryApplication.updateGoodsGuessCache(goodsId);
+    }
+
+    @Override
+    protected String[] listensTo() {
+        return new String[]{"cn.m2c.scm.domain.model.goods.event.GoodsOffShelfEvent",
+                "cn.m2c.scm.domain.model.goods.event.GoodsDeleteEvent"};
+    }
+}
