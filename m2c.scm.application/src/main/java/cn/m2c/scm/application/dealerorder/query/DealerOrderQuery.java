6770024483b64929831a3877a0bec79d4778149e
fanjc <dmkitwm@126.com>
2017-11-22 18:08:55 +0800

    add status judge.

--- m2c.scm.application/src/main/java/cn/m2c/scm/application/dealerorder/query/DealerOrderQuery.java
+++ m2c.scm.application/src/main/java/cn/m2c/scm/application/dealerorder/query/DealerOrderQuery.java
@@ -231,7 +231,7 @@ public class DealerOrderQuery {
                 .append(" a.rev_person, a.rev_phone, a.goods_amount, a.order_freight, a.plateform_discount, a.dealer_discount, a.order_id, af.after_sell_order_id\r\n")
                 .append(" , af.reject_reason, af.order_type, af.back_money FROM t_scm_order_detail dtl \r\n")
                 .append(" LEFT OUTER JOIN t_scm_order_dealer a ON dtl.dealer_order_id = a.dealer_order_id\r\n")
-                .append(" LEFT OUTER JOIN t_scm_order_after_sell af ON af.dealer_order_id = dtl.dealer_order_id AND af.sku_id=dtl.sku_id\r\n")
+                .append(" LEFT OUTER JOIN t_scm_order_after_sell af ON af.dealer_order_id = dtl.dealer_order_id AND af.sku_id=dtl.sku_id AND af._status NOT IN(-1, 3)\r\n")
                 .append(" LEFT OUTER JOIN t_scm_order_main om ON dtl.order_id = om.order_id\r\n")
                 .append(" WHERE a.dealer_id = ?  \r\n");
 
