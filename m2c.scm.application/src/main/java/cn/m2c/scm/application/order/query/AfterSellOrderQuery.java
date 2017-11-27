c24805e59d69c99155299d9366d6a4fbeb87ceb5
fanjc <dmkitwm@126.com>
2017-11-27 17:52:55 +0800

    gogo

--- m2c.scm.application/src/main/java/cn/m2c/scm/application/order/query/AfterSellOrderQuery.java
+++ m2c.scm.application/src/main/java/cn/m2c/scm/application/order/query/AfterSellOrderQuery.java
@@ -62,7 +62,7 @@ public class AfterSellOrderQuery {
 		StringBuilder sql = new StringBuilder();
 		sql.append(" SELECT ");
 		sql.append(
-				" after.after_sell_order_id,after.order_id,after.order_type,after.back_money,after._status,dealer.dealer_name,after.created_date ");
+				" after.after_sell_order_id,after.order_id,after.order_type,after.back_money, after.return_freight,after._status,dealer.dealer_name,after.created_date ");
 		sql.append(" FROM t_scm_order_after_sell after ");
 		sql.append(" LEFT JOIN t_scm_dealer dealer ON after.dealer_id = dealer.dealer_id");
 		sql.append(" LEFT JOIN t_scm_order_main main ON after.order_id = main.order_id ");
