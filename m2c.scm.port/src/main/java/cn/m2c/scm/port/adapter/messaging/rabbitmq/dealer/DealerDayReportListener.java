036d4a414dbeae4560f47b97ad1f63690a658f3e
owen <huc@lcjh.com>
2017-12-21 15:14:34 +0800

    数据统计

--- m2c.scm.port/src/main/java/cn/m2c/scm/port/adapter/messaging/rabbitmq/dealer/DealerDayReportListener.java
+++ m2c.scm.port/src/main/java/cn/m2c/scm/port/adapter/messaging/rabbitmq/dealer/DealerDayReportListener.java
@@ -1,11 +1,12 @@
 package cn.m2c.scm.port.adapter.messaging.rabbitmq.dealer;
 
+import cn.m2c.common.JsonUtils;
 import cn.m2c.ddd.common.application.configuration.RabbitmqConfiguration;
 import cn.m2c.ddd.common.event.ConsumedEventStore;
-import cn.m2c.ddd.common.notification.NotificationReader;
 import cn.m2c.ddd.common.port.adapter.messaging.rabbitmq.ExchangeListener;
 import cn.m2c.scm.application.dealer.DealerApplication;
 import cn.m2c.scm.application.dealer.command.DealerDayReportCommand;
+import com.alibaba.fastjson.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +14,7 @@ import org.springframework.orm.hibernate4.HibernateTransactionManager;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Map;
 
 /**
  * 商家数据按天报表
@@ -35,16 +37,22 @@ public class DealerDayReportListener extends ExchangeListener {
     @Override
     protected void filteredDispatch(String aType, String aTextMessage) throws Exception {
         LOGGER.info("DealerDayReportListener aTextMessage =>" + aTextMessage);
-        NotificationReader reader = new NotificationReader(aTextMessage);
-        String dealerId = reader.eventStringValue("dealerId");
-        String type = reader.eventStringValue("type");
-        Integer num = reader.eventIntegerValue("num");
-        Long money = reader.eventLongValue("money");
-        Date time = reader.eventDateValue("time");
+        JSONObject object = JSONObject.parseObject(aTextMessage);
+        JSONObject eventObject = object.getJSONObject("event");
+        String type = eventObject.getString("type");
+        Date time = eventObject.getDate("time");
         SimpleDateFormat df = new SimpleDateFormat("yyyyMMdd");
         Integer day = Integer.parseInt(df.format(time));
-        DealerDayReportCommand command = new DealerDayReportCommand(dealerId, type, num, money, day);
-        dealerApplication.saveDealerDayReport(command);
+        JSONObject infoObject = eventObject.getJSONObject("dealerInfo");
+        Map<String, String> dealerInfoMap = JsonUtils.toMap(JSONObject.toJSONString(infoObject));
+        for (Map.Entry<String, String> entry : dealerInfoMap.entrySet()) {
+            String dealerId = entry.getKey();
+            Map info = JsonUtils.toMap(entry.getValue());
+            Integer num = null != info.get("num") ? Integer.parseInt(info.get("num").toString()) : 0;
+            Long money = null != info.get("money") ? Long.parseLong(info.get("money").toString()) : 0l;
+            DealerDayReportCommand command = new DealerDayReportCommand(dealerId, type, num, money, day);
+            dealerApplication.saveDealerDayReport(command);
+        }
     }
 
     @Override
