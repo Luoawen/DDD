6f1613b3879e7f5a2936837b8b62e65b3a144658
Luoqingqwen <lqw51802@qq.com>
2018-01-04 17:05:18 +0800

    commit

--- m2c.scm.application/src/main/java/cn/m2c/scm/application/dealerorder/data/bean/DealerOrderAfterSellDetailBean.java
+++ m2c.scm.application/src/main/java/cn/m2c/scm/application/dealerorder/data/bean/DealerOrderAfterSellDetailBean.java
@@ -85,12 +85,24 @@ public class DealerOrderAfterSellDetailBean {
 	@ColumnAlias(value = "sku_id")
 	private String skuId;
 	
+	@ColumnAlias(value = "express_code")
+	private String expressCode;
+	
 	private String dealerId;
 	
 	
 	public Integer getDoStatus() {
 		return doStatus;
 	}
+	
+	public String getExpressCode() {
+		return expressCode;
+	}
+
+	public void setExpressCode(String expressCode) {
+		this.expressCode = expressCode;
+	}
+
 
 	public void setDoStatus(Integer doStatus) {
 		this.doStatus = doStatus;
