a69f2ef3b97b190997ef41482b26f32f188eb1f9
lux <lux@lcjh.com>
2018-01-06 15:49:09 +0800

    app拍照进入商品详情页，有特惠价则展示特惠价专属商品角标(未打开)

--- m2c.scm.application/src/main/java/cn/m2c/scm/application/goods/query/data/representation/app/AppGoodsDetailRepresentation.java
+++ m2c.scm.application/src/main/java/cn/m2c/scm/application/goods/query/data/representation/app/AppGoodsDetailRepresentation.java
@@ -2,6 +2,7 @@ package cn.m2c.scm.application.goods.query.data.representation.app;
 
 import cn.m2c.common.JsonUtils;
 import cn.m2c.scm.application.comment.query.data.bean.GoodsCommentBean;
+import cn.m2c.scm.application.config.data.bean.ConfigBean;
 import cn.m2c.scm.application.goods.query.data.bean.GoodsBean;
 import cn.m2c.scm.application.goods.query.data.bean.GoodsGuaranteeBean;
 import cn.m2c.scm.application.goods.query.data.bean.GoodsSkuBean;
@@ -47,6 +48,7 @@ public class AppGoodsDetailRepresentation {
     private String customerTel;
     private Map goodsSpecial;
     private List<Map> coupons;
+    private Map configs;
 
     public AppGoodsDetailRepresentation(GoodsBean bean, List<GoodsGuaranteeBean> goodsGuaranteeBeans,
                                         String goodsUnitName, String mresId, Integer commentTotal, GoodsCommentBean goodsCommentBean,
@@ -335,5 +337,18 @@ public class AppGoodsDetailRepresentation {
 
     public void setCoupons(List<Map> coupons) {
         this.coupons = coupons;
+    }
+    
+    public void setConfigs(List<ConfigBean> configsList) {
+    	if(null != configsList && configsList.size()>0) {
+    		this.configs = new HashMap<>();
+    		for(ConfigBean bean : configsList) {
+    			this.configs.put(bean.getConfigKey(), bean.getConfigValue());
+    		}
+    	}
+    }
+    
+    public Map getConfigs() {
+    	return configs;
     }
 }
