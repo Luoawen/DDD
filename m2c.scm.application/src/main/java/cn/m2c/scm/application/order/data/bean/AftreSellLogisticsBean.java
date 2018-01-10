a477a73ecfd303460f8fe483966f0aa3b25cbf69
Luoqingqwen <lqw51802@qq.com>
2018-01-10 14:33:19 +0800

    售后物流信息添加字段

--- m2c.scm.application/src/main/java/cn/m2c/scm/application/order/data/bean/AftreSellLogisticsBean.java
+++ m2c.scm.application/src/main/java/cn/m2c/scm/application/order/data/bean/AftreSellLogisticsBean.java
@@ -48,6 +48,36 @@ public class AftreSellLogisticsBean {
 	@ColumnAlias(value = "order_type")
 	private Integer orderType;
 	
+	/**
+	 * 配送方式 0:物流，1自有物流 
+	 */
+	@ColumnAlias(value = "express_way")
+	private Integer expressWay;
+
+	/**
+	 * 送货人姓名
+	 */
+	@ColumnAlias(value = "express_person")
+	private String expressPerson;
+	
+	/**
+	 * 送货人电话
+	 */
+	@ColumnAlias(value = "express_phone")
+	private String expressPhone;
+	
+	/**
+	 * 说明
+	 */
+	@ColumnAlias(value = "express_note")
+	private String expressNote;
+	
+	/**
+	 * 发货时间
+	 */
+	@ColumnAlias(value = "express_time")
+	private String expressTime;
+	
 	
 	/**
 	 * 商品信息
@@ -88,6 +118,47 @@ public class AftreSellLogisticsBean {
 
 	public Integer getStatus() {
 		return status;
+	}
+
+	
+	public Integer getExpressWay() {
+		return expressWay;
+	}
+
+	public String getExpressPerson() {
+		return expressPerson;
+	}
+
+	public String getExpressPhone() {
+		return expressPhone;
+	}
+
+	public String getExpressNote() {
+		return expressNote;
+	}
+
+	public String getExpressTime() {
+		return expressTime;
+	}
+
+	public void setExpressWay(Integer expressWay) {
+		this.expressWay = expressWay;
+	}
+
+	public void setExpressPerson(String expressPerson) {
+		this.expressPerson = expressPerson;
+	}
+
+	public void setExpressPhone(String expressPhone) {
+		this.expressPhone = expressPhone;
+	}
+
+	public void setExpressNote(String expressNote) {
+		this.expressNote = expressNote;
+	}
+
+	public void setExpressTime(String expressTime) {
+		this.expressTime = expressTime;
 	}
 
 	public String getAfterSellOrderId() {
