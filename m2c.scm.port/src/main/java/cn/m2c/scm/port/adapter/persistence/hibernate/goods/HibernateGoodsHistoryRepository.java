41bf946fc2611525b68d217501ad2198189dbad1
owen <huc@lcjh.com>
2018-01-18 17:28:57 +0800

    商品变更历史

--- m2c.scm.port/src/main/java/cn/m2c/scm/port/adapter/persistence/hibernate/goods/HibernateGoodsHistoryRepository.java
+++ m2c.scm.port/src/main/java/cn/m2c/scm/port/adapter/persistence/hibernate/goods/HibernateGoodsHistoryRepository.java
@@ -3,10 +3,12 @@ package cn.m2c.scm.port.adapter.persistence.hibernate.goods;
 import cn.m2c.ddd.common.port.adapter.persistence.hibernate.HibernateSupperRepository;
 import cn.m2c.scm.domain.model.goods.GoodsHistory;
 import cn.m2c.scm.domain.model.goods.GoodsHistoryRepository;
+import org.springframework.stereotype.Repository;
 
 /**
  * 商品审核变更历史记录
  */
+@Repository
 public class HibernateGoodsHistoryRepository extends HibernateSupperRepository implements GoodsHistoryRepository {
     @Override
     public void save(GoodsHistory goodsHistory) {
