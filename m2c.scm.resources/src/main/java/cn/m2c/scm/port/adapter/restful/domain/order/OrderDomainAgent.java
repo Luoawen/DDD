173a6c9842df60bfb2b2b75a79757475fdefa657
owen <huc@lcjh.com>
2018-03-06 11:37:27 +0800

    获取在某个用户下过的订单数

--- m2c.scm.resources/src/main/java/cn/m2c/scm/port/adapter/restful/domain/order/OrderDomainAgent.java
+++ m2c.scm.resources/src/main/java/cn/m2c/scm/port/adapter/restful/domain/order/OrderDomainAgent.java
@@ -1,8 +1,14 @@
 package cn.m2c.scm.port.adapter.restful.domain.order;
 
-import java.util.HashMap;
-import java.util.List;
-
+import cn.m2c.common.MCode;
+import cn.m2c.common.MResult;
+import cn.m2c.scm.application.order.SaleAfterOrderApp;
+import cn.m2c.scm.application.order.data.bean.DealerOrderMoneyInfoBean;
+import cn.m2c.scm.application.order.data.bean.RefundEvtBean;
+import cn.m2c.scm.application.order.data.representation.DealerOrderMoneyInfoRepresentation;
+import cn.m2c.scm.application.order.data.representation.OrderNums;
+import cn.m2c.scm.application.order.query.OrderQuery;
+import cn.m2c.scm.domain.NegativeException;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,16 +21,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-
-import cn.m2c.common.MCode;
-import cn.m2c.common.MResult;
-import cn.m2c.scm.application.order.SaleAfterOrderApp;
-import cn.m2c.scm.application.order.data.bean.DealerOrderMoneyInfoBean;
-import cn.m2c.scm.application.order.data.bean.RefundEvtBean;
-import cn.m2c.scm.application.order.data.representation.DealerOrderMoneyInfoRepresentation;
-import cn.m2c.scm.application.order.data.representation.OrderNums;
-import cn.m2c.scm.application.order.query.OrderQuery;
-import cn.m2c.scm.domain.NegativeException;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /***
@@ -88,6 +86,25 @@ public class OrderDomainAgent {
 		}
     	return new ResponseEntity<MResult>(result, HttpStatus.OK);
     }
+
+	/**
+	 * 获取在某个用户下过的订单数
+	 */
+	@RequestMapping(value="/get/user/before/num", method = RequestMethod.GET)
+	public ResponseEntity<MResult> getOrdersByUserId(@RequestParam(value="mainOrderId", required=false) String mainOrderId){
+		MResult result = new MResult(MCode.V_1);
+		try {
+			Integer num = orderQuery.getUserBeforeOrders(mainOrderId);
+			result.setContent(num);
+			result.setStatus(MCode.V_200);
+		}
+		catch (Exception e) {
+			LOGGER.info("获取用户以前下单数失败,e:" + e.getMessage());
+			result.setStatus(MCode.V_400);
+			result.setContent("获取用户以前下单数失败");
+		}
+		return new ResponseEntity<MResult>(result, HttpStatus.OK);
+	}
     
     /**
      * 获取多个商家订单的金额列表
