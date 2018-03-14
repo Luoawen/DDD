49425f6c3ca6557ce7cc89edf2858f92c90e0147
lux <lux@lcjh.com>
2018-03-14 14:44:53 +0800

    给营销组提供根据商品名模糊查询商品接口

--- m2c.scm.resources/src/main/java/cn/m2c/scm/port/adapter/restful/domain/goods/GoodsDomainAgent.java
+++ m2c.scm.resources/src/main/java/cn/m2c/scm/port/adapter/restful/domain/goods/GoodsDomainAgent.java
@@ -7,6 +7,7 @@ import cn.m2c.scm.application.goods.query.GoodsQueryApplication;
 import cn.m2c.scm.application.goods.query.data.bean.GoodsBean;
 import cn.m2c.scm.application.goods.query.data.representation.GoodsDetailMultipleRepresentation;
 import cn.m2c.scm.application.goods.query.data.representation.GoodsForNormalRepresentation;
+import cn.m2c.scm.application.goods.query.data.representation.GoodsRandomRepresentation;
 import cn.m2c.scm.application.goods.query.data.representation.domain.GoodsDetailRepresentation;
 import cn.m2c.scm.application.order.data.bean.CouponDealerBean;
 import cn.m2c.scm.application.order.data.representation.CouponDealerBeanRepresentation;
@@ -146,5 +147,32 @@ public class GoodsDomainAgent {
 			result = new MPager(MCode.V_400, "查询优惠券适用商品或商家失败");
 		}
 		return new ResponseEntity<MPager>(result, HttpStatus.OK);
+	}
+	
+	/**
+	 * 给营销工具提供接口根据商品名称模糊查询商品信息
+	 * @param goodsName  商品名称
+	 * @return
+	 */
+	@RequestMapping(value = "/name", method = RequestMethod.GET)
+	public ResponseEntity<MResult> queryGoodsMessageByGoodsName(
+			@RequestParam(value = "goodsName", required = false) String goodsName 
+			){
+		MResult result = new MResult(MCode.V_1);
+		try {
+			List<GoodsBean> list = goodsQueryApplication.queryGoodsMessageByGoodsName(goodsName);
+			if(null != list && list.size() > 0) {
+				List<GoodsRandomRepresentation> representations = new ArrayList<>();
+				for(GoodsBean bean : list) {
+					representations.add(new GoodsRandomRepresentation(bean));
+				}
+				result.setContent(representations);
+			}
+			result.setStatus(MCode.V_200);
+		}catch(Exception e) {
+			LOGGER.error("queryGoodsMessageByGoodsName Exception e:", e);
+			result = new MPager(MCode.V_400, "查询商品信息失败");
+		}
+		return new ResponseEntity<MResult>(result, HttpStatus.OK);
 	}
 }
