d898b58fac8e6dbc073b0044b25eb2f025a84364
leo <yezp@lcjh.com>
2018-03-16 15:54:06 +0800

    c

--- m2c.scm.resources/src/main/java/cn/m2c/scm/port/adapter/restful/web/order/OrderAgent.java
+++ m2c.scm.resources/src/main/java/cn/m2c/scm/port/adapter/restful/web/order/OrderAgent.java
@@ -620,7 +620,7 @@ public class OrderAgent {
     		LOGGER.info("shopName---------" + shopName);
     		String _attach= request.getHeader("attach");
 //			List<Integer> times = orderapplication.importExpressModel(myFile,userId,shopName,0,_attach);
-			List<Map<String, Integer>> countResult = orderapplication.importExpress(myFile,userId,shopName,dealerId,0,_attach);
+			List<Map<String, Object>> countResult = orderapplication.importExpress(myFile,userId,shopName,dealerId,0,_attach);
 			result.setContent(countResult);
 		} catch (NegativeException e) {
 			LOGGER.error("上传发货模板出错", e);
