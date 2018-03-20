172a8433feacbeb455128d54c76162ef4552dec1
lux <lux@lcjh.com>
2018-03-20 15:16:08 +0800

    给营销组提供的根据优惠券id查询适用商家/商品拆分成领域调用和前端调用

--- m2c.scm.resources/src/main/java/cn/m2c/scm/port/adapter/restful/admin/goods/AdminGoodsOutAgent.java
+++ m2c.scm.resources/src/main/java/cn/m2c/scm/port/adapter/restful/admin/goods/AdminGoodsOutAgent.java
@@ -1,10 +1,17 @@
 package cn.m2c.scm.port.adapter.restful.admin.goods;
 
 import cn.m2c.common.MCode;
+import cn.m2c.common.MPager;
 import cn.m2c.common.MResult;
 import cn.m2c.scm.application.goods.query.GoodsQueryApplication;
 import cn.m2c.scm.application.goods.query.data.bean.GoodsBean;
+import cn.m2c.scm.application.goods.query.data.representation.GoodsDetailMultipleRepresentation;
 import cn.m2c.scm.application.goods.query.data.representation.GoodsRandomRepresentation;
+import cn.m2c.scm.application.order.data.bean.CouponDealerBean;
+import cn.m2c.scm.application.order.data.representation.CouponDealerBeanRepresentation;
+import cn.m2c.scm.domain.service.goods.GoodsService;
+
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,6 +24,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+
+import javax.annotation.Resource;
 
 /**
  * 商品对运营后台提供的接口
@@ -28,6 +38,8 @@ public class AdminGoodsOutAgent {
 	
 	@Autowired
 	GoodsQueryApplication goodsQueryApplication;
+	@Resource(name = "goodsRestService")
+	GoodsService goodsService;
 	
 	/**
 	 * 运营平台根据商品名模糊查询最早十个商品
@@ -56,4 +68,59 @@ public class AdminGoodsOutAgent {
 		return new ResponseEntity<MResult>(result, HttpStatus.OK);
 	}
 	
+	/**
+	 * 根据优惠券id查询适用商家/商品
+	 * @param couponId  优惠券id
+	 * @param pageNum   第几页
+	 * @param rows      每页多少行
+	 * @return
+	 */
+	@RequestMapping(value = "/coupon/apply", method = RequestMethod.GET)
+	public ResponseEntity<MPager> queryCouponApplyGoodsOrDealer(
+		@RequestParam(value = "couponId", required = false) String couponId, 
+		@RequestParam(value = "pageNum", required = false, defaultValue = "1") Integer pageNum,
+		@RequestParam(value = "rows", required = false, defaultValue = "30") Integer rows
+		){
+		MPager result = new MPager(MCode.V_1);
+		try {
+			Map couponMap = null;
+			if(StringUtils.isNotEmpty(couponId)) { //调用营销接口查询优惠券信息
+				couponMap = goodsService.getCouponRange(couponId);
+			}
+			Integer total = 0;
+			if(null != couponMap) {
+				Integer couponRangeType = (Integer) couponMap.get("rangeType");
+				//根据优惠券适用类型查询商品或商家总数
+				total = goodsQueryApplication.queryCouponApplyGoodsOrDealerTotal(couponMap, couponRangeType);
+				if(total > 0) {
+					if(couponRangeType == 1) {//查商家
+						List<CouponDealerBean> couponDealerBeans = goodsQueryApplication.queryCouponApplyDealer(couponMap, pageNum, rows);
+						if(null != couponDealerBeans && couponDealerBeans.size() > 0) {
+							List<CouponDealerBeanRepresentation> representations = new ArrayList<>();
+							for(CouponDealerBean couponDealerBean : couponDealerBeans) {
+								representations.add(new CouponDealerBeanRepresentation(couponDealerBean));
+							}
+							result.setContent(representations);
+						}
+					}
+					if(couponRangeType == 0 || couponRangeType == 2 || couponRangeType == 3 || couponRangeType == 4) {//查商品
+						List<GoodsBean> goodsBeans = goodsQueryApplication.appSearchGoods(null, null, null, null, null, null, null, couponMap, pageNum, rows);
+						if(null != goodsBeans && goodsBeans.size() > 0) {
+							List<GoodsDetailMultipleRepresentation> representations = new ArrayList<>();
+							for(GoodsBean goodsBean : goodsBeans) {
+								representations.add(new GoodsDetailMultipleRepresentation(goodsBean));
+							}
+							result.setContent(representations);
+						}
+					}
+				}
+			}
+			result.setPager(total, pageNum, rows);
+            result.setStatus(MCode.V_200);
+		} catch (Exception e) {
+			LOGGER.error("queryCouponApplyGoodsOrDealer Exception e:", e);
+			result = new MPager(MCode.V_400, "查询优惠券适用商品或商家失败");
+		}
+		return new ResponseEntity<MPager>(result, HttpStatus.OK);
+	}
 }
